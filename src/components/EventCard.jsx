fcb2c8694fccee5a458f553562cca81f32e6dd34
ALEX BUNEA <ALEX BUNEA@DESKTOP-DI4EC9V>
2023-09-16 12:41:47 +0300

    Buy tickets for an event

--- src/components/EventCard.jsx
+++ src/components/EventCard.jsx
@@ -1,10 +1,14 @@
 import React, { useState } from 'react';
 import { useStyles } from './styling/styles';
+import { addOrder } from '../api_calls/orders_calls';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 export const EventCard = ({event}) => { 
 
     const [numberOfTickets,setNumberOfTickets] = useState(0);
     const [selectedTicketCategory, setSelectedTicketCategory] = useState(event.ticketCategories[0]?.description || '');
+    const [priceLabelText,setPriceLabelText] = useState("Price:-");
 
     function generateTicketOptions() {
         const eventTickets = event.ticketCategories;
@@ -19,6 +23,59 @@ export const EventCard = ({event}) => {
           </select>
         );
       }
+
+    function handleBuyTicket()
+    {
+      
+      const ticketList = event['ticketCategories'];
+      let ticketCategoryId = -1;
+      ticketList.forEach(ticket => {if(ticket.description === selectedTicketCategory) ticketCategoryId = ticket.id;})
+    
+      let order = {
+        eventID: event.eventID,
+        ticketCategoryID: ticketCategoryId,
+        numberOfTickets: parseInt(numberOfTickets)
+      }
+    
+      try {
+        addOrder(order).then(
+          response => {
+            if (response.ok) {
+              
+              setNumberOfTickets(0);
+              setPriceLabelText("Price:-");
+              toast('Success!');
+            } else {
+              response.json().then(errorData => {
+                toast(errorData.message);
+              });
+              
+            }
+          }
+        )   
+      }  
+       catch (error) {
+        console.error('Error adding order:', error);
+      }
+    }
+
+    function handleChangeTicketNumber(e) {
+      const newNumberOfTickets = e.target.value;
+    
+      const ticketList = event['ticketCategories'];
+      let price = 0;
+      ticketList.forEach((ticket) => {
+        if (ticket.description === selectedTicketCategory) {
+          price = ticket.price;
+        }
+      });
+    
+      const totalPrice = newNumberOfTickets * price;
+    
+      setNumberOfTickets((prevNumberOfTickets) => newNumberOfTickets);
+      setPriceLabelText("Price: " + totalPrice);
+    }
+    
 
     return(
         <div className='event-card rounded-lg bg-gray-200 p-4 m-4'>
@@ -42,12 +99,12 @@ export const EventCard = ({event}) => {
                 {generateTicketOptions()}
                 <div className="ticket-number-select">
                     <label htmlFor="tickets">Number of tickets:</label>
-                    <input type="number" name="tickets" value={numberOfTickets} min="1" max="100" onChange={(e) => setNumberOfTickets(e.target.value)}/>
-                    <p className="price-label">Price:-</p>
-                    <button className={[...useStyles('standard_button'), 'buy-ticket-btn'].join(' ')}>Confirm purchase</button>
+                    <input type="number" name="tickets" value={numberOfTickets} min="1" max="100" onChange={(e) => handleChangeTicketNumber(e)}/>
+                    <p className="price-label">{priceLabelText}</p>
+                    <button className={[...useStyles('standard_button'), 'buy-ticket-btn'].join(' ')} onClick={() => handleBuyTicket()}>Confirm purchase</button>
                 </div>
             </div>
-
+            <ToastContainer/>
         </div>
     );
 
