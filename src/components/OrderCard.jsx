8357bd3da86bcac881eaf6aadbce19de10c6965b
ALEX BUNEA <ALEX BUNEA@DESKTOP-DI4EC9V>
2023-09-16 13:54:46 +0300

    Update order

--- src/components/OrderCard.jsx
+++ src/components/OrderCard.jsx
@@ -1,25 +1,47 @@
 import React, { useEffect, useState } from 'react';
-import { useStyles } from './styling/styles';
+import { tailwindStyles } from './styling/styles';
 import { getEventById } from '../api_calls/events_calls';
 import './styling/OrderCard.css';
-import { deleteOrder } from '../api_calls/orders_calls';
+import { deleteOrder, updateOrder } from '../api_calls/orders_calls';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 export const OrderCard = ({order,onRemove}) => {
 
     const [numberOfTicketsSelected,setNumberOfTicketsSelected] = useState(order.numberOfTickets)
     const [initialTicketCategory,setInitialTicketCategory] = useState(order.ticketCategory?.description || '')
-    const [selectedTicketCategory, setSelectedTicketCategory] = useState(order.ticketCategory?.description || '');
+    const [initialNumberOfTickets,setInitialNumberOfTickets] = useState(order.numberOfTickets)
+    const [ticketCategorySelected, setTicketCategorySelected] = useState(order.ticketCategory?.description || '');
     const [event,setEvent] = useState(null);
-    
+    const [priceLabelText,setPriceLabelText] = useState("Price:" + order.totalPrice + "$");
+    const [isUpdateButtonDisabled,setIsUpdateButtonDisabled] = useState(true);
+    const [updateButtonStyle,setUpdateButtonStyle] = useState(tailwindStyles('disabled_button').join(' '))
+
     useEffect(() => {
         getEventById(order.eventID).then(data => {setEvent(data)});
     },[])
+
+
+    useEffect(() => {
+      
+      if(initialNumberOfTickets != numberOfTicketsSelected || ticketCategorySelected != initialTicketCategory)
+      {
+        setIsUpdateButtonDisabled(false)
+        setUpdateButtonStyle(tailwindStyles('standard_button').join(' '));
+      }
+      else
+      {
+        setIsUpdateButtonDisabled(true)
+        setUpdateButtonStyle(tailwindStyles('disabled_button').join(' '));
+      }
+
+    },[initialTicketCategory,initialNumberOfTickets,numberOfTicketsSelected,ticketCategorySelected])
 
     function generateTicketOptions() {
         const eventTickets = event?.ticketCategories;
         
         return (
-          <select className="ticket-category" value={selectedTicketCategory} onChange={(e) => {setSelectedTicketCategory(e.target.value)}}>
+          <select className="ticket-category" value={ticketCategorySelected} onChange={(e) => {setTicketCategorySelected(e.target.value)}}>
             {eventTickets?.map((ticket, index) => (
               <option key={index} value={ticket.description}>
                 {ticket.description}
@@ -37,6 +59,35 @@ export const OrderCard = ({order,onRemove}) => {
         });
     }
 
+    function handleUpdateOrder()
+    {
+      
+      const patchRequestBody = {
+        numberOfTickets: numberOfTicketsSelected,
+        ticketType: ticketCategorySelected
+      }
+    
+      updateOrder(order.orderID,patchRequestBody)
+      .then(response => {
+
+        if (response.ok) {
+
+            response.json().then(data => {
+              setInitialNumberOfTickets(data.numberOfTickets);
+              setInitialTicketCategory(data.ticketCategory.description);
+              setPriceLabelText("Price:" +  data.totalPrice + "$")
+              toast('Success!');
+            })   
+
+        } else {
+          response.json().then(errorData => {
+            toast(errorData.errorMessage);
+          });
+        }  
+        
+      });
+    }
+
     return ( 
         <div className='order-card rounded-lg bg-gray-200 p-4 m-4'>
 
@@ -45,7 +96,7 @@ export const OrderCard = ({order,onRemove}) => {
                 <p>Type:{event?.eventType}</p>
                 <p>Venue:{event?.venue.location}</p>
                 <p>Date: {event?.startDate} - {event?.endDate}</p>
-                <p>Price: {order.totalPrice}$</p>
+                <p>{priceLabelText}</p>
             </div>
 
             <div className='orders-actions'>
@@ -64,18 +115,18 @@ export const OrderCard = ({order,onRemove}) => {
                 />
                 </div>
 
-                <button className={[...useStyles('standard_button'), 'delete-order-btn'].join(' ')} onClick={() => handleDeleteOrder()}>
+                <button className={[...tailwindStyles('standard_button'), 'delete-order-btn'].join(' ')} onClick={() => handleDeleteOrder()}>
                     Delete
                 </button>
 
-                <button className={[...useStyles('disabled_button'), 'update-order-btn'].join(' ')}>
+                <button className={[updateButtonStyle, 'update-order-btn'].join(' ')} onClick={() => handleUpdateOrder()} disabled={isUpdateButtonDisabled}>
                     Update
                 </button>
 
             </div>
 
             
-
+            <ToastContainer/>
         </div>
      );
 }
